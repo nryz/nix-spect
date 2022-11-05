ef1b530a2ad8ff50a0aee9608a7c5e31820210c4
unknown <mail@nryz.xyz>
2022-11-05 14:55:36 +0000

    initial prototype

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,55 @@
-use rnix::types::*;
-use std::{env, fs};
+use clap::{arg, command};
+use std::process::Command;
 
-fn main() {
-    let mut iter = env::args().skip(1).peekable();
+fn get_completions(path: String) -> Result<Vec<String>, String> {
+    let result = Command::new("nix")
+        .arg("eval")
+        .arg("--raw")
+        .arg(&path)
+        .env("NIX_GET_COMPLETIONS", "3")
+        .output()
+        .expect("command failed to run");
 
-    if iter.peek().is_none() {
-        eprintln!("Usage: nix-specter <file>");
-        return;
+    if !result.status.success() {
+        let error = String::from_utf8(result.stderr).unwrap();
+        return Err(format!("Error: {}", error));
     }
 
-    for file in iter {
-        let content = match fs::read_to_string(file) {
-            Ok(content) => content,
-            Err(err) => {
-                eprintln!("Error reading file: {}", err);
-                return;
-            }
-        };
+    let out = String::from_utf8(result.stdout).unwrap();
 
-        let ast = rnix::parse(&content);
-
-        for error in ast.errors() {
-            println!("Error: {}", error);
+    let mut completions: Vec<String> = Vec::new();
+    for s in out.lines() {
+        if !s.eq("attrs") {
+            completions.push(s.to_string().replace(&path, ""));
         }
+    }
+    Ok(completions)
+}
 
-        println!("{}", ast.root().dump());
+fn main() {
+    let matches = command!()
+        .arg(arg!([flake] "flake path").required(true))
+        .get_matches();
+
+    let flake_path = matches
+        .get_one::<String>("flake")
+        .expect("expected a valid flake path");
+
+    let mut path: String = flake_path.to_owned();
+
+    if !path.contains('#') {
+        path.push('#');
+    }
+
+    if !path.ends_with('.') && !path.ends_with('#') {
+        path.push('.');
+    }
+
+    println!("whole_path: {}", path);
+
+    let completions = get_completions(path).expect("Error while getting completions");
+
+    for c in completions {
+        println!("completion: {}", c);
     }
 }
